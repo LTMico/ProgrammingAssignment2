87e5e19415ba6bbf3941990d3c932c05650a44b2
Lindsay Mico <lindsaymico@gmail.com>
2015-08-20 12:40:25 -0700

    HW 2 R Programming Course (Coursera)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-Programming (r-prog 31) COursera HW 2
+## Key concept is Lexical Scoping
+## Completed 8-20-15 Lindsay Thomas Mico 
 
-## Write a short comment describing this function
 
+## Generates a list containing 4 functions to define and retrieve a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() m
+        list(set = set, get = get,
+                setinv = setinv,
+                getinv = getinv)
+        
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## If the inverse is already defined it is pulled from the list defined in makeCacheMatrix
+## If the inverse is not yer defined it is calculated using the solve function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
+
